d3833a202d4a78e87630fef42541602827e5ac6b
haeun1700 <haeun7700@hs.ac.kr>
2024-08-22 17:26:11 +0900

    login.js, Login.css, Signup.css, Signup.js 수정

--- hospital_platform/hospital_platform/src/main/frontend/src/components/Signup.js
+++ hospital_platform/hospital_platform/src/main/frontend/src/components/Signup.js
@@ -29,103 +29,104 @@ function SignUp() {
         e.preventDefault();
 
         if (formData.password !== formData.confirmPassword) {
-            setError("Passwords do not match");
+            setError("비밀번호가 일치하지 않습니다.");
             return;
         }
 
         try {
             await axios.post("http://localhost:8080/api/signup", formData);
-            alert("Sign up successful!");
             navigate("/login");
         } catch (error) {
             console.error("There was an error signing up!", error.response?.data || error.message);
-            setError(`Sign up failed! ${error.response?.data || error.message}`);
+            setError(`회원가입에 실패했습니다. ${error.response?.data || error.message}`);
         }
     };
 
     return (
-        <div className="signup-container">
-            <div className="signup-header">
-                <h2>회원가입</h2>
-            </div>
-            <form onSubmit={handleSubmit} className="signup-form">
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    type="password"
-                    name="confirmPassword"
-                    placeholder="Confirm Password"
-                    value={formData.confirmPassword}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    type="text"
-                    name="nickname"
-                    placeholder="Nickname"
-                    value={formData.nickname}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    type="text"
-                    name="phoneNum"
-                    placeholder="Phone Number"
-                    value={formData.phoneNum}
-                    onChange={handleChange}
-                    required
-                />
-                <div className="address-container">
-                    <label>주소</label>
+        <div className="signup-body">
+            <div className="signup-container">
+                <div className="signup-header">
+                    <h2>회원가입</h2>
+                </div>
+                <form onSubmit={handleSubmit} className="signup-form">
                     <input
-                        type="text"
-                        name="address1"
-                        placeholder="Address1"
-                        value={formData.address1}
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        placeholder="Confirm Password"
+                        value={formData.confirmPassword}
                         onChange={handleChange}
                         required
                     />
                     <input
                         type="text"
-                        name="address2"
-                        placeholder="Address2"
-                        value={formData.address2}
+                        name="name"
+                        placeholder="Name"
+                        value={formData.name}
                         onChange={handleChange}
+                        required
                     />
-                </div>
-                <input
-                    type="text"
-                    name="zipcode"
-                    placeholder="Zipcode"
-                    value={formData.zipcode}
-                    onChange={handleChange}
-                    required
-                />
-                <button type="submit">회원가입</button>
-                {error && <p className="error-message">{error}</p>}
-            </form>
+                    <input
+                        type="text"
+                        name="nickname"
+                        placeholder="Nickname"
+                        value={formData.nickname}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="phoneNum"
+                        placeholder="Phone Number"
+                        value={formData.phoneNum}
+                        onChange={handleChange}
+                        required
+                    />
+                    <div className="address-container">
+                        <label>주소</label>
+                        <input
+                            type="text"
+                            name="address1"
+                            placeholder="Address1"
+                            value={formData.address1}
+                            onChange={handleChange}
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="address2"
+                            placeholder="Address2"
+                            value={formData.address2}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <input
+                        type="text"
+                        name="zipcode"
+                        placeholder="Zipcode"
+                        value={formData.zipcode}
+                        onChange={handleChange}
+                        required
+                    />
+                    <button type="submit">회원가입</button>
+                    {error && <p className="error-message">{error}</p>}
+                </form>
+            </div>
         </div>
     );
 }
